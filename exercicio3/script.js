a172b901181dee1818216ad15b9267b191655357
francinaldapmfaria <portela854@gmail.com>
2022-08-30 15:32:10 -0300

    correção exercício 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,7 +1,7 @@
 const questaoA = 5 > 20 && 5 < 2
 const questaoB = 5 === 5 || 5 === "5"
-const questaoC = !20 > 50
-const questaoD = !20 > 50 || 50> 60
+const questaoC = !(20 > 50)
+const questaoD = !(20 > 50 || 50> 60)
 
 console.log(questaoA)
 console.log(questaoB)
