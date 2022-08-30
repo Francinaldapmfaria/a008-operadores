fffcaa49b5292c38ca45635acefd6fd9c4dfe999
francinaldapmfaria <portela854@gmail.com>
2022-08-30 14:48:30 -0300

    exercício 3 feito

---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+const questaoA = 5 > 20 && 5 < 2
+const questaoB = 5 === 5 || 5 === "5"
+const questaoC = !20 > 50
+const questaoD = !20 > 50 || 50> 60
+
+console.log(questaoA)
+console.log(questaoB)
+console.log(questaoC)
+console.log(questaoD)
+
