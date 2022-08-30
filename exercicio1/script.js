0de39b41d3632ecc5b7a1223529da51ca230837a
francinaldapmfaria <portela854@gmail.com>
2022-08-30 13:49:09 -0300

    Exercício 1 realizado com sucesso

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let numeropar = Number(prompt("Insira um número par"))
+
+console.log (numeropar)
+console.log (numeropar % 2)
+// quando número par o resto sempre será 0, e quando impar o resto é 1.
+
+
